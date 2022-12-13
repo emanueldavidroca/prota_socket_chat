5236581b78eb33dd860688ebdbf69747cce88770
emanuel <emanueldavidroca@gmail.com>
2022-12-13 01:49:12 -0300

    cors

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const app = express();
 const http = require("http").Server(app)
 const port = process.env.PORT || 8080;
 const io = require("socket.io")(http);
+var cors = require('cors')
+app.use(cors())
+
+app.use(cors())
 io.on("connection",socket=>{})
 app.get("/",(req,res)=>{
 	res.json("get request");
