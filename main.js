1f8ac2ea281a6bbbdee43813a5d455fbda1dcc4e
emanuel <emanueldavidroca@gmail.com>
2022-12-13 01:59:05 -0300

    cors

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ const http = require("http").Server(app)
 const port = process.env.PORT || 8080;
 const io = require("socket.io")(http);
 var cors = require('cors')
-app.use(cors())
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
-app.use(cors())
-io.on("connection",socket=>{})
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 app.get("/",(req,res)=>{
 	res.json("get request");
+})
+io.on("connection",socket=>{
+	console.log(socket)
 })
 app.listen(port,()=>{
 	console.log(`escuchando port: ${port}`);
