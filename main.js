8a258ed6787c9909566dbcce2e6ec3ad3c4e9c1f
emanuel <emanueldavidroca@gmail.com>
2022-12-14 01:07:15 -0300

    websocket3

--- main.js
+++ main.js
@@ -1,33 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const port = process.env.PORT || 8080;
-var cors = require('cors')
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
-
-app.get("/",(req,res)=>{
-	res.json("get request");
-})
-// add this
-app.get('/socket.io/socket.io.js', (req, res) => {
-  res.sendFile(__dirname + '/node_modules/socket.io/client-dist/socket.io.js');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
-///
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(port, () => {
-  console.log(`listening on *:${port}`);
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
