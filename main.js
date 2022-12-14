c06c56dd054a7ad7c844066bf64791c9c333d4c7
emanuel <emanueldavidroca@gmail.com>
2022-12-14 01:48:41 -0300

    websocket3

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+const io = new Server(server,{
+  cors: {
+    origin: "https://prota.ar"
+  }
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
-
+io.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
