49b0c41c91e4bdc7c658185a95f13a970c8171d5
emanuel <emanueldavidroca@gmail.com>
2022-12-15 14:31:37 -0300

    prota

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
-//falta
-const io = new Server(server,{
+const httpServer = app.listen(3000, () => {console.log(`Server listening on port 3000`)});
+const io = new Server(httpServer,{
   cors: {
     origin: "https://prota.ar:* "
   }
@@ -18,15 +17,9 @@ app.get("/nuevo",(req,res)=>{
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-    });
-  socket.on('salutations', (msg) => {
-      console.log('message: ' + msg);
-    });
+    console.log('message: ' + msg);
+  });
 });
 io.on("connect_error", (err) => {
   console.log(`connect_error due to ${err.message}`);
-});
-server.listen(3000, () => {
-  console.log('listening on *:3000');
 });
