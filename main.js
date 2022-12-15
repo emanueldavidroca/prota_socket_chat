78426efced6ca5cf1c34f204d3d9d9bf3df42fc8
emanuel <emanueldavidroca@gmail.com>
2022-12-15 01:36:28 -0300

    socket 4

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server,{
+const httpServer = app.listen(3000, () => {console.log(`Server listening on port ${port}`)});
+const io = new Server(httpServer,{
   cors: {
-    origin: "https://prota.ar"
+    origin: "https://prota.ar:* "
   }
 });
 app.get("",(req,res)=>{
