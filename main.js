776e19087f35535b4ecb38ad02cff9a5ddf2758f
emanuel <emanueldavidroca@gmail.com>
2022-12-16 00:30:43 -0300

    cambios nuevos

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-//falta
 const io = new Server(server,{
   cors: {
-    origin: "https://prota.ar:* "
+    origin: "https://prota.ar"
   }
 });
 app.get("",(req,res)=>{
@@ -20,15 +19,12 @@ app.get("/nuevo",(req,res)=>{
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-    });
-  socket.on('salutations', (msg) => {
-      console.log('message: ' + msg);
-    });
+    console.log('message: ' + msg);
+  });
 });
 io.on("connect_error", (err) => {
   console.log(`connect_error due to ${err.message}`);
 });
-server.listen(port, () => {
-  console.log(`listening on *:${port}`);
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
