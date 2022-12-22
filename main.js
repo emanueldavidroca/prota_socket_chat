12d62301b2c62c2b25928ff850d39cf091e3957a
emanuel <emanueldavidroca@gmail.com>
2022-12-22 01:45:02 -0300

    nuevo socket

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ app.get("/nuevo",(req,res)=>{
     idChat:req.query.idChat,
     date:req.query.date
   };
+  console.log(req.query)
   io.broadcast.to(session_id).emit("chat_message",JSON.stringify(data));
 });
 
