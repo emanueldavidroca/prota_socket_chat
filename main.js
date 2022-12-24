fe621cbd38392427c78c4e4135d327610e601623
emanuel <emanueldavidroca@gmail.com>
2022-12-24 01:03:25 -0300

    chat

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const io = new Server(server,{
 });
 app.get("/nuevo",(req,res)=>{
   let session_id = req.query.session_id;
+  console.log(req.query);
   const data = {
     message:req.query.idChat,
     rol:req.query.rol,
@@ -19,19 +20,19 @@ app.get("/nuevo",(req,res)=>{
     idChat:req.query.idChat,
     date:req.query.date
   };
-  console.log(req.query)
+  console.log(req.query);
   io.broadcast.to(session_id).emit("chat_message",JSON.stringify(data));
 });
 
 io.on('connection', (socket) => {
   socket.on("join",(msg)=>{
-    console.log(msg.session_id);
+    console.log("chat abierto:"+msg.session_id);
     socket.join(msg.session_id);
   });
   console.log(socket.handshake.query);
   console.log('a user connected');
   socket.on('chat_message', (msg) => {
-    console.log('message:');
+    console.log('message:'+msg);
   });
 });
 
